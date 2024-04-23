b1839fbc9a6ab923fcf19fc5bb9c790360103f4c
techmefr <compigni.gaetan@gmail.com>
2024-04-23 16:35:12 +0200

    init fetch

--- Weatherify/src/components/Weather.jsx
+++ Weatherify/src/components/Weather.jsx
@@ -1,7 +1,26 @@
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-function Weather(){
+function Weather() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.WEATHER_API_KEY}`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <div></div>;
 }
+
 export default Weather;
